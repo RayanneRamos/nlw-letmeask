ee088532ab4f27612a80438590f4350f2fa7c5d5
Rayanne Ramos <43352880+RayanneRamos@users.noreply.github.com>
2021-07-02 14:50:11 -0300

    Criação do componente CardQuestion

---
+++ src/components/CardQuestion/index.tsx
@@ -0,0 +1,50 @@
+import { ReactNode } from 'react';
+import cx from 'classnames';
+
+import '../CardQuestion/style.scss';
+import { useTheme } from '../../hooks/useTheme';
+
+type QuestionProps = {
+  content: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  children?: ReactNode;
+  isAnswered: boolean;
+  isHighlighted: boolean;
+  amountLike?: number;
+}
+
+export function CardQuestion({
+  content,
+  author,
+  isAnswered = false,
+  isHighlighted = false,
+  amountLike,
+  children,
+}: QuestionProps) {
+  const { theme } = useTheme();
+
+  return(
+    <div
+      className={cx(
+        `question ${theme}`,
+        { answered: isAnswered },
+        { highlighted: isHighlighted }
+      )}
+    >
+      <div className="content">
+        { amountLike !== undefined && (<span className="likes">{amountLike} {amountLike > 1 ? "likes" : "like"}</span>) }
+      </div>
+      <p>{content}</p>
+      <footer>
+        <div className="user-info">
+          <img src={author.avatar} alt={author.name} />
+          <span>{author.name}</span>
+        </div>
+        <div>{children}</div>
+      </footer>
+    </div>
+  );
+}
